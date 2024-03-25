339842e931c7b333f09cdc28e5da64d6a6ce0e2f
vovasobol1 <dengyrostov@gmail.com>
2024-03-25 23:07:06 +0300

    all models registered
    now shows the fields of each model in the admin panel

--- tests/tests/admin.py
+++ tests/tests/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from .models import Question, Test, Answer, TestUser, Grade, Solutions
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('author_id', 'subject_id', 'theme_id', 'times_solved' , 'expert_id')
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('test', 'question_text', 'correct_answer', 'addition_info' )
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer_text' )
+
+
+@admin.register(TestUser)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'test_id')
+
+
+@admin.register(Grade)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'test_id', 'question_id', 'answer' )
+
+@admin.register(Solutions)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id_result', 'answer', 'correct_answer' )
+
+
+
 # Register your models here.

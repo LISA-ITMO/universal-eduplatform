92bb7ebd1a12739540b0168178efbd8fd2a0cf86
vovasobol1 <103885272+vovasobol1@users.noreply.github.com>
2024-03-26 21:39:34 +0300

    Update models.py :added str methods to models for improved string representation.

--- subjects/courses/models.py
+++ subjects/courses/models.py
@@ -4,22 +4,37 @@ from django.urls import reverse
 class Subject(models.Model): #Таблица с данными предметов
     name_subject = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name_subject
+
 class Student(models.Model): #Таблица с данными учащихся
     id_student = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
     isExpert = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.id_student
+
 class Theme(models.Model): #Таблица с темами тестов
     name_theme = models.CharField(max_length=100)
     id_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name_theme
 
 class Course(models.Model):
     id_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     id_expert = models.ForeignKey(Student, on_delete=models.CASCADE)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.id_subject
+
 class Student_Course_Subject(models.Model):
     id_student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='course_id_student')
     id_subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='course_id_subject')
     id_expert = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='course_id_expert')
+
+    def __str__(self):
+        return self.id_expert

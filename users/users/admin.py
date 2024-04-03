105d8e233aa3273fba10ee402c460f201ce1df2c
verkalacheva <verkalacheva@mail.ru>
2024-04-04 00:29:23 +0300

    update admin users

--- users/users/admin.py
+++ users/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import User
 
-
-admin.site.register(User)
+@admin.register(User)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'role', 'is_active' , 'is_staff', 'is_superuser', 'created_at', 'updated_at')

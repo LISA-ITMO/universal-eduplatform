90533839cec0c031a26d062f819106c7bd7bbf8a
verkalacheva <verkalacheva@mail.ru>
2024-06-20 18:03:49 +0300

    fix compose analytics

--- analytics/project/settings.py
+++ analytics/project/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'corsheaders',
     'analytics',
+    'tests'
 ]
 
 MIDDLEWARE = [
--- tests/tests/models.py
+++ tests/tests/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Test(models.Model):
     author_id = models.IntegerField(null=False, blank=False)
     subject_id = models.IntegerField(null=False, blank=False)
@@ -8,16 +9,28 @@ class Test(models.Model):
     expert_id = models.IntegerField(default=0, null=False, blank=False)
     max_points = models.IntegerField(default=0, null=False, blank=False)
 
+    class Meta:
+        app_label = 'tests'
+
+
 class Question(models.Model):
     id_test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
     question_text = models.CharField(max_length=100, null=False, blank=False)
     addition_info = models.TextField(null=False, blank=False)
     question_points = models.IntegerField(default=0, null=False, blank=False)
 
+    class Meta:
+        app_label = 'tests'
+
+
 class Answer(models.Model):
     answer_text = models.TextField(null=False, blank=False)
     id_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
     is_correct = models.BooleanField()
+
+    class Meta:
+        app_label = 'tests'
+
 
 class Result(models.Model):
     id_user = models.IntegerField(null=False, blank=False)
@@ -26,8 +39,14 @@ class Result(models.Model):
     theme = models.TextField(null=False, blank=False)
     points_user = models.IntegerField(null=True, blank=False)
 
+    class Meta:
+        app_label = 'tests'
+
 
 class Solutions(models.Model):
     id_result = models.ForeignKey(Result, on_delete=models.CASCADE, related_name='solutions')
     id_question = models.ForeignKey(Question, on_delete=models.CASCADE)
     user_answer = models.TextField(null=False, blank=False)
+
+    class Meta:
+        app_label = 'tests'

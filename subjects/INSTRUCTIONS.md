85c08191bea9ef52e1bce40dde50803807fb187e
0rtemk <kavabanga@test.ru>
2026-03-04 23:45:35 +0300

    remove unneccesary files

--- subjects/INSTRUCTIONS.md
+++
@@ -1,8 +0,0 @@
-# Course Module
-## Инструкция
-
-1. ``cd Quiz-platform/subjects``
-2. Обновить docker, если нужно ``sudo snap refresh docker --channel=latest/edge``, либо установить
-3. ``sudo docker-compose -f docker-compose.dev.yml up --build``
-
-``swagger/`` - swagger
--- subjects/courses/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-#Registering models
-from .models import Subject, Theme, Course, Student_Course_Subject
-
-admin.site.register(Subject)
-admin.site.register(Theme)
-admin.site.register(Course)
-admin.site.register(Student_Course_Subject)
